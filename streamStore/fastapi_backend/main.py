940df7b6c4a298eae44e0596fb0ad24b9fb39b8e
aastha <asingla1101@gmail.com>
2025-11-02 09:18:32 +0530

    added producer code to the add to cart api

--- streamStore/fastapi_backend/main.py
+++ streamStore/fastapi_backend/main.py
@@ -6,6 +6,20 @@ import uuid
 from fastapi import FastAPI, HTTPException
 from typing import List
 import sqlite3
+from confluent_kafka import Producer
+import json
+
+producer_config = {
+    'bootstrap.servers': 'localhost:9092'
+}
+
+producer = Producer(producer_config)
+
+def delivery_report(err, msg):
+    if err:
+        print(f"❌ Order Failed: {err}")
+    else:
+        print(f"✅ Order Delivered to topic: {msg.topic()} partition: {msg.partition()} offset: {msg.offset()} message value: {msg.value()}")
 
 app = FastAPI()
 
@@ -24,7 +38,7 @@ class CheckoutRequest(BaseModel):
 @app.post("/checkout")
 async def checkout(order_data: CheckoutRequest):
     order_id = str(uuid.uuid4())  # Generate order ID 
-    
+  
     try:
         for item in order_data.items:
             product_id = item.product_id
@@ -34,18 +48,34 @@ async def checkout(order_data: CheckoutRequest):
             if quantity <= 0:
                 raise HTTPException(status_code=400, detail=f"Invalid quantity for product {product_id}")
             
-            # Get current price before updating inventory
             unit_price = query_individual_item_price('ecommerce.db', product_id)
             if unit_price is None:
                 raise HTTPException(status_code=404, detail=f"Product {product_id} not found")
             
-            # Update inventory
-            success = update_inventory_quantity('ecommerce.db', product_id, quantity)
-            if not success:
-                raise HTTPException(status_code=400, detail=f"Insufficient inventory for product {product_id}")
+            # dict for kafka
+            order = {
+                "order_id": order_id,
+                'product_id': product_id,
+                'quantity': quantity,
+                'unit_price': unit_price
+            }
             
-            # Add sale record
-            add_sale('ecommerce.db', order_id, product_id, quantity, unit_price)
+            value = json.dumps(order).encode('utf-8')
+            producer.produce(
+                topic='orders', 
+                value=value,
+                callback=delivery_report 
+            )
+            
+            producer.flush()
+            
+            # # Update inventory
+            # success = update_inventory_quantity('ecommerce.db', product_id, quantity)
+            # if not success:
+            #     raise HTTPException(status_code=400, detail=f"Insufficient inventory for product {product_id}")
+            
+            # # Add sale record
+            # add_sale('ecommerce.db', order_id, product_id, quantity, unit_price)
         
         return {"order_id": order_id, "status": "success", "message": "Checkout completed successfully"}
     
